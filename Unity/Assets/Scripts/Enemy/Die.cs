ad94f6c68f5df914c97237ddec48c9aacc72c51b
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2019-11-09 14:29:14 +0100

    Added basic player slash ability, made enemies killable and made player look at camera #1 #3

---
+++ Unity/Assets/Scripts/Enemy/Die.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Die : MonoBehaviour
+{
+    Stats stats;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stats = GetComponent<Stats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stats.health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
