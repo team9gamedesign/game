384897c2273ca134396f30eaf622425a2cae9e86
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2019-11-13 13:25:31 +0100

    Added the earth shatter berserker ability #7

---
+++ Unity/Assets/Scripts/Abilities/Player/Berserker/EarthShatter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthShatter : MonoBehaviour
+{
+    public GameObject earthShatter;
+
+    public void InstantiateEarthShatter()
+    {
+        Instantiate(earthShatter, transform.position, transform.rotation);
+        Follow cameraFollow = Camera.main.GetComponent<Follow>();
+        cameraFollow.shake = true;
+        cameraFollow.shakeTimer = 0.2f;
+    }
+}
