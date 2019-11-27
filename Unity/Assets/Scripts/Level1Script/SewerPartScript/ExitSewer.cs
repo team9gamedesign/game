f60a13e4e9e126663983a1489003f0a0b3a7f946
Marcus Werlinder <werli@kth.se>
2019-11-27 18:36:21 +0100

    Done with the sewer part of the level, should be its own issue but I would group it togheter now with issue 18

---
+++ Unity/Assets/Scripts/Level1Script/SewerPartScript/ExitSewer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitSewer : MonoBehaviour
+{
+    public Camera camera;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            camera.GetComponent<SewerGlobalVars>().exitInfo();
+            if (Input.GetKeyDown("space"))
+            {
+                //Add code here to switch scenes
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            camera.GetComponent<SewerGlobalVars>().hideInfoBox();
+        }
+    }
+}
