aec82489a82e4a01d0e718a294e943879c0a2fc4
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2019-12-03 19:26:15 +0100

    Added Combination HUD #27

---
+++ Unity/Assets/Scripts/Player/CombinationUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombinationUI : MonoBehaviour
+{
+	public Image Combination1;
+	public Image Combination2;
+
+    public Color active;
+    public Color inActive;
+
+	private CombinationHandler combinationHandler;
+
+    void Start()
+	{
+		combinationHandler = GetComponent<CombinationHandler>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Combination1.color = combinationHandler.QKeyCombination != null ? active : inActive;
+        Combination2.color = combinationHandler.EKeyCombination != null ? active : inActive;
+    }
+}
