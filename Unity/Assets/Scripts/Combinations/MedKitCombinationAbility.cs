56331cb1c91c99ce0f700ff261b01e7d4a43327d
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2019-12-04 22:29:28 +0100

    Added a MedKit combination item #36

---
+++ Unity/Assets/Scripts/Combinations/MedKitCombinationAbility.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedKitCombinationAbility : MonoBehaviour
+{
+    public int healAmount = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Stats stats = GetComponent<Ability>().user.GetComponent<Stats>();
+        stats.ChangeHealth(healAmount);
+        Destroy(gameObject);
+    }
+}
