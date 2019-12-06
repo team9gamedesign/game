2a48bb42ffbc4fe7a044459bcd1c0d2be23c3c71
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2019-12-06 11:12:57 +0100

    Game is now reset upon boss completion

---
+++ Unity/Assets/Scripts/Enemy/BossDeath.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossDeath : MonoBehaviour
+{
+    public void OnDestroy()
+    {
+        if(GetComponent<Stats>().health <= 0)
+        {
+            SceneManager.LoadSceneAsync(0);
+            Destroy(PlayerManager.instance.player);
+            PlayerManager.instance.player = null;
+        }
+    }
+}
