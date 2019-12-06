f610d41d4f80a920871ab838f317523cb80ec755
vakeneller <vakeneller@gmail.com>
2019-12-06 10:33:38 +0100

    Fixed stuck-spot in sewers and boss-not-shooting bug

--- Unity/Assets/Scripts/Abilities/Enemy/ThrowBomb.cs
+++ Unity/Assets/Scripts/Abilities/Enemy/ThrowBomb.cs
@@ -23,10 +23,11 @@ public class ThrowBomb : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("ThrowBomb", false);
+        animator.SetBool("ShouldMove", true);
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
